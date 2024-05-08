0aaa95b76f86623f1f2dc9a97e20cc296aa7fb9f
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-05-08 17:31:33 +0200

    fix int test (mock vector store)

--- src/test/java/com/exxeta/ai/AiAzureApplicationTests.java
+++ src/test/java/com/exxeta/ai/AiAzureApplicationTests.java
@@ -1,13 +1,28 @@
 package com.exxeta.ai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.aot.DisabledInAotMode;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@DisabledInAotMode // MockBean not supported in AOT mode
 class AiAzureApplicationTests {
+
+    @MockBean
+    VectorStore vectorStore;
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void contextLoads() {
+        assertTrue(context.containsBean("chatController"));
     }
 
 }
